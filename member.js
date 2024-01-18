2fe0a301a22be5e1322ae204c0f363b6a473399c
Colin Wilkinson <wreckingadm@users.noreply.github.com>
2024-01-18 14:54:53 +0000

    Colpilot second commit!

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+
+    if (memberValue == "1") {
+        memberSkills.innerHTML = "<option value='1'>HTML</option><option value='2'>CSS</option><option value='3'>JavaScript</option><option value='4'>PHP</option><option value='5'>SQL</option><option value='6'>Python</option><option value='7'>C++</option><option value='8'>C#</option><option value='9'>Java</option><option value='10'>Ruby</option><option value='11'>Swift</option><option value='12'>C</option><option value='13'>Objective-C</option><option value='14'>Perl</option><option value='15'>R</option><option value='16'>Matlab</option><option value='17'>Scala</option><option value='18'>Go</option><option value='19'>Visual Basic</option><option value='20'>
